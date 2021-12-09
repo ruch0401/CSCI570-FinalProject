a36a7c43e5bf173c417673e96db3ba39d8c974af
ruch0401 <ruch0401@gmail.com>
2021-12-09 11:42:19 -0800

    Removed shabang from shell scripts

--- 1111417799_3695883753/src/1111417799_3695883753_basic.sh
+++ 1111417799_3695883753/src/1111417799_3695883753_basic.sh
@@ -1,4 +1,2 @@
-#!/bin/sh
-
 javac Basic_1111417799_3695883753.java
 java Basic_1111417799_3695883753 input.txt
--- 1111417799_3695883753/src/1111417799_3695883753_efficient.sh
+++ 1111417799_3695883753/src/1111417799_3695883753_efficient.sh
@@ -1,4 +1,2 @@
-#!/bin/sh
-
 javac Efficient_1111417799_3695883753.java
 java Efficient_1111417799_3695883753 input.txt
